a3ecee5682449b42ff08c504d08e8c62d33e6f24
Anderson N. Silva <anderson.nuneseth@gmail.com>
2019-08-09 00:28:38 -0300

    fix: Better code organization. LikeService created

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -3,10 +3,8 @@ const likeService = require('../services/LikeService');
 
 module.exports = {
     async store(request, response) {
-
-
         try {
-            likeService.createLike(request.headers.user, request.params.devId);
+            await likeService.createLike(request.headers.user, request.params.devId);
             return response
             .json({
                 status: 'success',
@@ -17,76 +15,8 @@ module.exports = {
             .status(400)
             .json({
                 status: 'error',
-                message: e
+                message: err
             });
         }
-        
-        const { user: idDevLoggedIn } = request.headers;
-        const { devId: idTargetDev } = request.params;
-
-        if (idDevLoggedIn === idTargetDev) {
-            return response
-            .status(400)
-            .json({
-                status: 'error',
-                message: 'You can not like yourself!'
-            });
-        }
-
-        const devLoggedIn = await Dev.findOne({_id: idDevLoggedIn}).catch(err => {
-            console.log('Logged dev not found');
-        });
-
-        if (!devLoggedIn) {
-            return response
-            .status(400)
-            .json( {
-                status: 'error',
-                message: 'Logged dev not found'
-            });
-        }
-        
-        const devTarget = await Dev.findOne({_id: idTargetDev}).catch( err => {
-            console.log('Target dev not found');
-        });
-
-        if (!devTarget) {
-            return response
-            .status(400)
-            .json( {
-                status: 'error',
-                message: 'Target dev not found'
-            })
-        }
-
-        if (devLoggedIn.likes.includes(devTarget._id)) {
-            return response
-            .status(400)
-            .json({
-                status: 'error',
-                message: 'You already liked this dev!'
-            })
-        }
-    
-        try {
-            
-            if (devTarget.likes.includes(devLoggedIn._id)) {
-                // TODO: Is this a match?
-            }
-
-            devLoggedIn.likes.push(idTargetDev);
-            devLoggedIn.save();
-        } catch (e) {
-            return response
-            .status(400)
-            .json({
-                status: 'error',
-                message: e
-            });
-        }
-        return response.json({
-            status: 'success',
-            data: true
-        });
     }
 }
--- src/services/LikeService.js
+++ src/services/LikeService.js
@@ -6,14 +6,15 @@ module.exports = {
 
     async createLike(loggedDevId, targetDevId) {
         
-        this.validateInput(loggedDevId, targetDevId);
+        await this.readDevsFromDatabase(loggedDevId, targetDevId);
+        
+        this.validateInput();
 
         try {
-            if (this.targetDev.likes.includes(this.loggedDev._id)) {
-                // TODO: Is it a match? yes... 
-            }
+            this.verifyMatch();
             this.loggedDev.likes.push(this.targetDev._id);
             await this.loggedDev.save();
+            return true;
         } catch (err) {
             console.log('An error ocour when user tried to like another one. Details: ', err);
             throw "Ops! The operation could not be complete! Sorry!";
@@ -21,20 +22,26 @@ module.exports = {
 
     },
 
-    async validateInput(loggedDevId, targetDevId) {
+    verifyMatch() {
+        if (this.targetDev.likes.includes(this.loggedDev._id)) {
+            // TODO: Is it a match? yes... 
+        }
+    },
 
-        if (loggedDevId == targetDevId) throw "You can't like yourself!";
+    validateInput() {
+        if (this.loggedDev._id == this.targetDev._id) throw "You can't like yourself!";
+        if (this.loggedDev.likes.includes(this.targetDev._id)) throw "You already liked this dev.";
+    },
 
+    async readDevsFromDatabase(loggedDevId, targetDevId) {
         this.loggedDev = await Dev.findOne({_id: loggedDevId}).catch(err => {
             console.log('Logged dev not found');
+            throw "Logged dev not found.";
         });
-        if (!this.loggedDev) throw "Logged dev not found!";
 
         this.targetDev = await Dev.findOne({ _id: targetDevId }).catch(err => {
             console.log('Target dev not found');
+            throw "Target dev not found.";
         });
-        if (!this.targetDev) throw "Target dev not found!";
-
-        if (this.loggedDev.likes.includes(this.targetDev._id)) throw "You already liked this dev.";
     }
 };
